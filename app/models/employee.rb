552887eaab8229f52f9ed24c3e21fcc9f7da8f9e
Brian Umbarger <brian.umbarger@gmail.com>
2014-11-25 13:40:11 -0500

    Working Authentication

--- app/models/employee.rb
+++
@@ -1,7 +0,0 @@
-class Employee
-  include Mongoid::Document
-  field :name, type: String
-  field :email, type: String
-  field :password_digest, type: String
-  field :has_secure_password, type: Boolean
-end
