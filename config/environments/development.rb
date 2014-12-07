c0c2311e0af2799ffc464def5c7ceae9bb46588c
Brian Umbarger <brian.umbarger@gmail.com>
2014-12-06 22:47:46 -0500

    got more working

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -15,7 +15,7 @@ Rails.application.configure do
 
   # Don't care if the mailer can't send.
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
-  config.action_mailer.raise_delivery_errors = false
+  #config.action_mailer.raise_delivery_errors = false
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
@@ -32,4 +32,15 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.smtp_settings = { :address => "smtp.gmail.com",
+                                         :port => 587,
+                                         :domain => "gmail.com",
+                                         :user_name => "brian.umbarger@gmail.com",
+                                         :password => "xyz123xyz123!!",
+                                         :authentication => 'plain',
+                                         :enable_starttls_auto => true }
 end
